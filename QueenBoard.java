82c1718c5cf7b5eb6b0795e58eb5a59c004ae3ad
Oliver Ripps <oripps@stuy.edu>
2019-02-07 09:37:43 -0500

    created constructor that initiates the board

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,6 +1,9 @@
 public class QueenBoard{
-  private int[][]board;
-  public QueenBoard(int size){}
+  private int[][] board;
+  public QueenBoard(int size){
+    int[][] b = new int[size][size];
+    board=b;
+  }
   private boolean addQueen(int r, int c)
   private boolean removeQueen(int r, int c)
   public String toString(){}
