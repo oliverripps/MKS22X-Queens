8cccd0a5a991170ed1bb42f2d1c295c657734e84
Oliver Ripps <oripps@stuy.edu>
2019-02-07 09:35:23 -0500

    added in all function headers from website

---
+++ QueenBoard.java
@@ -0,0 +1,18 @@
+public class QueenBoard{
+  private int[][]board;
+  public QueenBoard(int size){}
+  private boolean addQueen(int r, int c)
+  private boolean removeQueen(int r, int c)
+  public String toString(){}
+  public boolean solve(){}
+  public int countSolutions(){}
+
+
+
+
+
+
+
+
+
+}
