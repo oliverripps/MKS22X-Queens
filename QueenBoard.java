90fb2c2ae32d3281d30cede52c620e36b8464415
Oliver Ripps <oripps@stuy.edu>
2019-02-07 10:48:45 -0500

    fixed add queen

--- QueenBoard.java
+++ QueenBoard.java
@@ -10,19 +10,33 @@ public class QueenBoard{
     }
   }
   private boolean addQueen(int r, int c)}{
-    board[r,c]=-1;
-    for(int i=1;i<board.length;i++){
-      board[r+i][c]+=1;
+    if(board[r][c]==0){
+      board[r,c]=-1;
+      for(int i=1;i<board.length;i++){
+        board[r+i][c]+=1;
+      }
+      for(int i=1;i<board[r].length;i++){
+        board[r][c+i]+=1;
+      }
+      for(int i=1;i<board[r].length;i++){
+        board[r+i][c+i]+=1;
+      }
+    return true;
+  }
+    else{
+      return false;
     }
-    for(int i=1;i<board[r].length;i++){
-      board[r][c+i]+=1;
-    }
-    for(int i=1;i<board[r].length;i++){
-      board[r+i][c+i]+=1;
+  private boolean removeQueen(int r, int c){
+    return true;
+  }
+  public String toString(){
+    String str="";
+    for(int i=0;i<board.length;i++){
+      for(int l=0;l<board[r].length;l++){
+        board[i][l]==0;
+      }
     }
   }
-  private boolean removeQueen(int r, int c)
-  public String toString(){}
   public boolean solve(){}
   public int countSolutions(){}
 
