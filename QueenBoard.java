fb6cc780a438efbfdc51dee886e0ba22feb02fe8
Oliver Ripps <oripps@stuy.edu>
2019-02-07 10:44:04 -0500

    chanegd constructor to add 0s and started add queens

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,9 +3,18 @@ public class QueenBoard{
   public QueenBoard(int size){
     int[][] b = new int[size][size];
     board=b;
+    for(int i=0;i<board.length;i++){
+      for(int l=0;l<board[r].length;l++){
+        board[i][l]==0;
+      }
+    }
   }
   private boolean addQueen(int r, int c)}{
     board[r,c]=-1;
+    for(int i=0;i<board.length;i++){
+      board[i][c]+=1
+    }
+
   }
   private boolean removeQueen(int r, int c)
   public String toString(){}
