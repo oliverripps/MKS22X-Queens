37ef4b5c9b0a740001cfc287092edb528cd25067
Oliver Ripps <oripps@stuy.edu>
2019-02-08 13:01:16 -0500

    testing toString

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver{
   public static void main(String[] args){
     QueenBoard b = new QueenBoard(8);
+    b.addQueen(5,4);
     System.out.println(b.toString());
   }
 
--- QueenBoard.java
+++ QueenBoard.java
@@ -3,16 +3,17 @@ public class QueenBoard{
   public QueenBoard(int size){
     int[][] b = new int[size][size];
     board=b;
-    for(int i=0;i<board.length;i++){
-      for(int l=0;l<board[r].length;l++){
-        board[i][l]==0;
+    for(int i=0;i<size;i++){
+      for(int l=0;l<size;l++){
+        board[i][l]=0;
       }
     }
   }
-  private boolean addQueen(int r, int c)}{
+
+  public boolean addQueen(int r, int c){//make private
     if(board[r][c]==0){
-      board[r,c]=-1;
-      for(int i=1;i<board.length;i++){
+      board[r][c]=-1;
+      /*for(int i=1;i<board.length-1;i++){
         board[r+i][c]+=1;
       }
       for(int i=1;i<board[r].length;i++){
@@ -20,29 +21,31 @@ public class QueenBoard{
       }
       for(int i=1;i<board[r].length;i++){
         board[r+i][c+i]+=1;
-      }
+      }*/
     return true;
   }
     else{
       return false;
     }
-  private boolean removeQueen(int r, int c){
-    return true;
   }
+
+  /*private boolean removeQueen(int r, int c){
+    return true;
+  }*/
   public String toString(){
     String str="";
     for(int i=0;i<board.length;i++){
-      for(int l=0;l<board[r].length;l++){
+      for(int l=0;l<board[0].length;l++){
         if(board[i][l]==-1){
           str+="Q";
         }
         if(board[i][l]!=-1){
           str+=board[i][l];
         }
-        if(l<board[r].length-1){
+        if(l<board[0].length-1){
           str+=" ";
         }
-        if(l>=board[r].length-1){
+        if(l>=board[0].length-1){
           str+="\n";
         }
 
@@ -50,8 +53,9 @@ public class QueenBoard{
     }
     return str;
   }
-  public boolean solve(){}
-  public int countSolutions(){}
+
+  /*public boolean solve(){}
+  public int countSolutions(){}*/
 
 
 
